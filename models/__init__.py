74d7a6f7c163ccd600ecff145f2db50ea6a4acaf
dhevialeuys <dhevialeuys@gmail.com>
2022-03-14 01:06:41 +0700

    First Commit

---
+++ models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from . import models
+from . import partner
+from . import panggung
+from . import pelaminan
+from . import kursipengantin
+from . import kursitamu
---
+++ models/kursitamu.py
@@ -0,0 +1,13 @@
+from odoo import api, fields, models
+
+
+class KursiTamu(models.Model):
+    _name = 'wedding.kursitamu'
+    _description = 'Data Tentang Kursi Tamu dan Harganya'
+
+    name = fields.Char(string='Name')
+    tipe = fields.Selection(string='Tipe Kursi', selection = [('plastik', 'Plastik'), ('stainless', 'Stainless')])
+    stok = fields.Integer(string='Stok Kursi')
+    harga = fields.Integer(string='Harga Sewa')
+    
+    
